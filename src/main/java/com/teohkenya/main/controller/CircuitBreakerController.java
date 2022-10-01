ef1d65770c01998100fc5094e0d9ce57f2f1a92e
KEN20960 <muchirinephat5@gmail.com>
2022-10-01 10:24:32 +0200

    Circuit Breaker

--- src/main/java/com/teohkenya/main/controller/CircuitBreakerController.java
+++ src/main/java/com/teohkenya/main/controller/CircuitBreakerController.java
@@ -1,6 +1,7 @@
 package com.teohkenya.main.controller;
 
 import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
  * @ Author NMuchiri
  **/
 @RestController
+@Slf4j
 public class CircuitBreakerController {
 
 
@@ -18,7 +20,7 @@ public class CircuitBreakerController {
     @Retry(name = "default")
     @GetMapping("sample-api")
     public String sampleApi(){
-
+        log.info("SAMPLE API CALL RECEIVED");
         ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
                 String.class);
 
